b8762948cca3c3496b14de034c9abf6c5e427913
Myles Smith <hello@itsmyl.es>
2024-02-25 21:22:12 +0000

    Update default.blade.php

--- resources/views/components/actions/default.blade.php
+++ resources/views/components/actions/default.blade.php
@@ -1,4 +1,4 @@
-<x-tiffey::button {{ $actionComponent->getAction($data) }} {{ $actionComponent->getColor() }}>
+<x-tiffey::button {{ $actionComponent->getAction($data) }} >
     <x-tiffey::icon icon="{{ $actionComponent->icon }}" label="{{ $actionComponent->title }}" />
     @if($actionComponent->showLabel)
         {{ $actionComponent->title }}
