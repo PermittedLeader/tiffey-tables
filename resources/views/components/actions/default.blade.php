327ec2973e2693d04c98db777722159a4ab0d581
Myles Smith <hello@itsmyl.es>
2024-02-25 10:40:41 +0000

    Update default.blade.php

--- resources/views/components/actions/default.blade.php
+++ resources/views/components/actions/default.blade.php
@@ -1,23 +1,14 @@
 @if($actionComponent->route)
-<x-tiffey::button
-    href="{{ $actionComponent->getRoute($data) }}"
-    >
-    <x-tiffey::icon icon="{{ $actionComponent->icon }}" label="{{ $actionComponent->title }}" />
-</x-tiffey::button>
+    <x-tiffey::button
+        href="{{ $actionComponent->getRoute($data) }}"
+        >
+        <x-tiffey::icon icon="{{ $actionComponent->icon }}" label="{{ $actionComponent->title }}" />
+    </x-tiffey::button>
 @else
-@if(is_object($data))
-        <x-tiffey::button wire:click="attach('{{ addslashes(get_class($data)) }}','{{ $data->id }}')" color="bg-success-light">
-            <x-tiffey::icon icon="{{ $actionComponent->icon }}" label="{{ $actionComponent->title }}" />
-            @if($actionComponent->showLabel)
-                {{ $actionComponent->title }}
-            @endif
-        </x-tiffey::button>
-    @else
-        <x-tiffey::button wire:click="{{ $actionComponent->action }}" color="bg-success-100">
-            <x-tiffey::icon icon="{{ $actionComponent->icon }}" label="{{ $actionComponent->title }}" />
-            @if($actionComponent->showLabel)
-                {{ $actionComponent->title }}
-            @endif
-        </x-tiffey::button>
-    @endif
+    <x-tiffey::button wire:click="{{ $actionComponent->action }}" >
+        <x-tiffey::icon icon="{{ $actionComponent->icon }}" label="{{ $actionComponent->title }}" />
+        @if($actionComponent->showLabel)
+            {{ $actionComponent->title }}
+        @endif
+    </x-tiffey::button>
 @endif
