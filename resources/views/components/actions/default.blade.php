41d11ee6d30928645dc123cd4a5b6b40b2e77e67
Myles Smith <hello@itsmyl.es>
2024-02-25 22:03:38 +0000

    update to use blade attributebag

--- resources/views/components/actions/default.blade.php
+++ resources/views/components/actions/default.blade.php
@@ -1,4 +1,5 @@
-<x-tiffey::button {{ $actionComponent->getAction($data) }} >
+@props([])
+<x-tiffey::button {{ $attributes->merge($actionComponent->getAction($data)) }} >
     <x-tiffey::icon icon="{{ $actionComponent->icon }}" label="{{ $actionComponent->title }}" />
     @if($actionComponent->showLabel)
         {{ $actionComponent->title }}
