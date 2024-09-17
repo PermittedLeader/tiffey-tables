2ca2d176ce2a0e683bd3e0bcf83140678c12eef4
Myles Smith <159888845+PermittedLeader@users.noreply.github.com>
2024-09-17 20:56:58 +0100

    Belongs to many table better handles HasMany relations

--- src/Http/Livewire/BelongsToManyTable.php
+++ src/Http/Livewire/BelongsToManyTable.php
@@ -4,12 +4,16 @@ namespace Permittedleader\TablesForLaravel\Http\Livewire;
 
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Permittedleader\TablesForLaravel\Http\Livewire\Table;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Livewire\WithoutUrlPagination;
 use Permittedleader\TablesForLaravel\View\Components\Actions\Action;
 
 abstract class BelongsToManyTable extends Table
 {
-    protected $listeners = [];
+    use WithoutUrlPagination;
+
+    protected $listeners = ['detach'];
 
     public bool $isSearchable = false;
 
@@ -23,39 +27,56 @@ abstract class BelongsToManyTable extends Table
 
     public Model $model;
 
-    public abstract function query(): Builder;
+    abstract public function query(): Builder;
 
-    public abstract function columns(): array;
+    abstract public function columns(): array;
 
     public function actions(): array
     {
         return [
-            Action::makeAction(function($data){
+            Action::makeAction(function ($data) {
                 return 'attach('.$data->id.')';
-            },'Attach')->showLabel()->gate(function($data){
-                return !$this->model->{$this->relationshipName}?->contains($data);
+            }, 'Attach')->showLabel()->gate(function ($data) {
+                return ! $this->model->{$this->relationshipName}?->contains($data);
             })->icon('fa-solid fa-link'),
-            Action::makeAction(function($data){
+            Action::makeAction(function ($data) {
                 return 'detach('.$data->id.')';
-            },'Detach')->showLabel()->gate(function($data){
+            }, 'Detach')->showLabel()->gate(function ($data) {
                 return $this->model->{$this->relationshipName}?->contains($data);
-            })->icon('fa-solid fa-link-slash')
+            })->icon('fa-solid fa-link-slash'),
         ];
     }
 
-    public function attach($modelKey){
-        $this->model->{$this->relationshipName}()->attach($modelKey);
+    public function attach($modelKey)
+    {
+        $relationship = $this->model->{$this->relationshipName}();
 
-        self::success('You have successfully attached these items.','Attached',bag: $this->getMessageBagName());
+        if ($relationship instanceof BelongsToMany) {
+            $relationship->attach($modelKey);
+        } elseif ($relationship instanceof HasMany) {
+            $relatedModel = $relationship->getRelated()->find($modelKey);
+            $relatedModel->{$relationship->getForeignKeyName()} = $this->model->id;
+            $relatedModel->save();
+        }
+
+        self::success('You have successfully attached these items.', 'Attached', bag: $this->getMessageBagName());
 
         $this->dispatch('refreshParent');
     }
 
     public function detach($modelKey)
     {
-        $this->model->{$this->relationshipName}()->detach($modelKey);
+        $relationship = $this->model->{$this->relationshipName}();
 
-        self::success('You have successfully detached these items.','Detached',bag: $this->getMessageBagName());
+        if ($relationship instanceof BelongsToMany) {
+            $relationship->detach($modelKey);
+        } elseif ($relationship instanceof HasMany) {
+            $relatedModel = $relationship->getRelated()->find($modelKey);
+            $relatedModel->{$relationship->getForeignKeyName()} = null;
+            $relatedModel->save();
+        }
+
+        self::success('You have successfully detached these items.', 'Detached', bag: $this->getMessageBagName());
 
         $this->dispatch('refreshParent');
     }
